2649d2f5aa9477e3da78eed46251b225d2d33f88
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-06-27 15:21:11 +0300

    UserForm

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,6 +2,13 @@ import css from './App.module.css';
 import UserForm from '../UserForm/UserForm';
 
 export default function App() {
-  return <div className={css.container}><h1>Forms in React</h1>
-  <UserForm/></div>;
+  const addUser = (newUser) => {
+    console.log("Send to backend", newUser);
+  };
+  return (
+    <div className={css.container}>
+      <h1>Forms in React</h1>
+      <UserForm onAdd={addUser} />
+    </div>
+  );
 }
--- src/Components/UserForm/Userform.jsx
+++ src/Components/UserForm/Userform.jsx
@@ -1,14 +1,21 @@
 import css from "./UserForm.module.css";
 
-export default function UserForm() {
+export default function UserForm({ onAdd }) {
   //у формі є сабміт,  передаємо handleSubmit, за змовчуванням event
   const handleSubmit = (event) => {
     event.preventDefault();
-    const form = event.target.elements;
-    console.log(form.username.value, form.role.value);
+    const form = event.target;
+    onAdd({
+      username: form.elements.username.value,
+      role: form.elements.role.value,
+    });
+
+    form.reset();
     //console.log(event.turget.elements.username.value) так в неконтрольованій формі отримують значення
     // <=так зібрали поточні значення
     // console.log(handleSubmit);
+    //завдання форми, збір логіки даних, все що туди пишуть
+    //івент(event) Не повинен залишати компонент форми
   };
   return (
     <form className={css.form} onSubmit={handleSubmit}>
