c08418fb28542fe550767af98335e4128ae88b09
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-06-27 14:48:56 +0300

    form

--- src/Components/UserForm/Userform.jsx
+++ src/Components/UserForm/Userform.jsx
@@ -1,10 +1,15 @@
 import css from "./UserForm.module.css";
 
 export default function UserForm() {
+  //у формі є сабміт,  передаємо handleSubmit, за змовчуванням event
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(handleSubmit);
+  };
   return (
-    <form className={css.form}>
+    <form className={css.form} onSubmit={handleSubmit}>
       <div className={css.grup}>
-        <label> Username:</label>
+        <label>Username:</label>
         <input type="text" name="username"></input>
       </div>
       <div className={css.grup}>
