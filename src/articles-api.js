2be4dd07b61cacf11c5c89ff9a372a93eded2854
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-07-08 14:42:15 +0300

    fetchArticles

--- src/articles-api.js
+++ src/articles-api.js
@@ -3,6 +3,14 @@ import axios from "axios";
 axios.defaults.baseURL = "http://hn.algolia.com/api/v1";
 
 export const fetchArticles = async (topic) => {
-    const res = await axios.get(`/search?query=${topic}`);
-    return res.data.hits;  
-}
+  //const res = await axios.get(`/search?query=${topic}&page=2&hitsPerPage=20`);
+    //можна як пер.вар, але краще через об'єкт парамс
+  const res = await axios.get("/search", {
+    params: {
+      query: topic,
+      page: 1,
+      hitsPerPage: 10,
+    },
+  });
+  return res.data.hits;
+};
